5e34ff4926fb26772e4e8a1d3574c4ee09c60682
Slendy Nicole Molina Ruiz <slendymolina@gmail.com>
2020-01-17 23:41:20 -0500

    Buscador de archivos

---
+++ Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace Buscador_de_Archivos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnCarpeta = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.btnBuscar = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // BtnCarpeta
+            // 
+            this.BtnCarpeta.Location = new System.Drawing.Point(307, 46);
+            this.BtnCarpeta.Name = "BtnCarpeta";
+            this.BtnCarpeta.Size = new System.Drawing.Size(164, 30);
+            this.BtnCarpeta.TabIndex = 0;
+            this.BtnCarpeta.Text = "Seleccionar Carpeta";
+            this.BtnCarpeta.UseVisualStyleBackColor = true;
+            this.BtnCarpeta.Click += new System.EventHandler(this.BtnCarpeta_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(43, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(244, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Seleccionar Ubicación de la carpeta";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 122);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(291, 24);
+            this.textBox1.TabIndex = 2;
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.Location = new System.Drawing.Point(354, 123);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 23);
+            this.btnBuscar.TabIndex = 0;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = true;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(43, 101);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(137, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Nombre del archivo";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 18;
+            this.listBox1.Location = new System.Drawing.Point(46, 168);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.ScrollAlwaysVisible = true;
+            this.listBox1.Size = new System.Drawing.Size(405, 130);
+            this.listBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(483, 314);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.BtnCarpeta);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnCarpeta;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button btnBuscar;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.IO;
+using Microsoft.VisualBasic.Devices;
+using System.Windows.Forms;
+
+namespace Buscador_de_Archivos
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        string CarpetaBusquedad="";
+
+        Computer Ju = new Computer();
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            if (CarpetaBusquedad=="")
+            {
+                MessageBox.Show("Falta seleccionar la carpeta");
+            }
+            else
+            {
+                Motor(CarpetaBusquedad);
+            }
+           
+        }
+
+        void Motor(string Mot)
+        {
+            string pop = Mot;
+
+            //Archivos
+            for (int i = 0; i < Directory.EnumerateFiles(Mot).Count(); i++)
+            {
+                FileInfo k = new FileInfo(Directory.GetFiles(Mot)[i]);
+                if (k.Name == textBox1.Text)
+                {
+                    listBox1.Items.Add("Archivo encontrado en: " + k.FullName);
+                }
+            }
+
+            //Carpetas
+            if (Directory.EnumerateDirectories(Mot).Count() > 0)
+            {
+                for (int i = 0; i < Directory.EnumerateDirectories(Mot).Count(); i++)
+                {
+                    Motor(Directory.GetDirectories(Mot)[i]);
+                }
+            }
+        }
+
+        private void BtnCarpeta_Click(object sender, EventArgs e)
+        {
+            //Crear Cuadro de Seleccionar Carpeta
+            FolderBrowserDialog Carpeta = new FolderBrowserDialog();
+
+            //Si la carpeta ha sido elegida
+            if (Carpeta.ShowDialog()==DialogResult.OK)
+            {
+                //Carpeta que hemos seleccionado
+                CarpetaBusquedad = Carpeta.SelectedPath;
+
+                //Verificar si es correcto
+                //MessageBox.Show(CarpetaBusquedad);
+            }
+        }
+    }
+}
